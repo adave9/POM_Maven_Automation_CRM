03f28a13e7e873e4d446f972d32f49a487e95ad4
Ankur Dave <ankurdave9@gmail.com>
2022-11-26 23:18:41 -0500

    First Commit.

---
+++ src/main/java/com/crm/qa/pages/Contacts.java
@@ -0,0 +1,14 @@
+package com.crm.qa.pages;
+
+import java.io.IOException;
+
+import com.crm.qa.base.TestBase;
+
+public class Contacts extends TestBase{
+
+	public Contacts() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
